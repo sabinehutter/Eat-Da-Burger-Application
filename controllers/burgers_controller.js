e1da1fe6ad81917c05ed802c2284534c5dea0e2b
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-04 21:45:13 -0600

    fixing bugs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,32 +2,27 @@ var express = require("express")
 
 var router = express.Router();
 
-var burger = require("../models/burger")
+var burger = require("../config/orm")
 
 router.get("/", function (req, res) {
     res.redirect("/burgers");
 });
 
 router.get("/burgers", function (req, res) {
-    burger.selectAll(function (data) {
-        var hbsObject = {
+    burger.all("burgers", function (data) {
+        res.render("index", {
             burger_data: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject)
+        });
     })
 });
 
 router.post("/burgers/create", function (req, res) {
-    burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function (result) {
-        // Send back the ID of the new quote
-        res.json({
-            id: result.insertId
-        });
+
+    console.log(req)
+    burger.create("burgers", req.body.burger_name, function (result) {
+        // wrapper for orm.js that using MySQL insert callback will return a log to console,
+        // render back to index with handle
+        console.log(result);
         res.redirect("/");
     });
 });
@@ -37,16 +32,14 @@ router.put("/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();}
-        })
-    });
+    burger.update("burgers",req.params.id, function (result) {
+        // wrapper for orm.js that using MySQL update callback will return a log to console,
+        // render back to index with handle
+        console.log(result);
+        // Send back response and let page reload from .then in Ajax
+        res.sendStatus(200);
+    })
+});
 
 
 module.exports = router
--- models/burger.js
+++ models/burger.js
@@ -7,16 +7,19 @@ var burger = {
             cb(res);
         });
     },
-    insertOne: function(cols,vals, cb){
-        orm.insertOne("burgers", cols, vals, function(res){
-            cb(res);
-        })
-    },
-    updateOne: function(objColVals, condition, cb){
-        orm.updateOne("burgers", objColVals, condition, function(res){
-            cb(res);
-        });
-    }
+    insertOne: function(name, cb) {
+        orm.create("burgers", [
+          "burger_name", "devoured"
+        ], [
+          name, false
+        ], cb);
+      },
+    updateOne: function(id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+          devoured: true
+        }, condition, cb);
+      }
 };
 
 module.exports = burger;
