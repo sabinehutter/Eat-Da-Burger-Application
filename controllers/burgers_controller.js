20d57bc8b0241f69d4761ed65dd83b5356ab2d20
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-04 20:30:28 -0600

    updating orm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,19 +19,34 @@ router.get("/burgers", function (req, res) {
 });
 
 router.post("/burgers/create", function (req, res) {
-    burger.create(req.body.burger_name, function (result) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
         // Send back the ID of the new quote
-        console.log(result);
+        res.json({
+            id: result.insertId
+        });
         res.redirect("/");
     });
 });
 
-router.put("/burgers/:id", function(req, res){
-    burger.update(req.params.id, function(result) {
-    
-        console.log(result);
-    
-    })
-})
+router.put("/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();}
+        })
+    });
+
 
 module.exports = router
