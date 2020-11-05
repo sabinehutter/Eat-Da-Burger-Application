7e0d40ee78c9a60127c295c00f8a184514ca2802
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-11-04 19:02:57 -0600

    finalizing controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,20 +1,36 @@
 var express = require("express")
 
-var  router = express.Router();
+var router = express.Router();
 
 var burger = require("../models/burger")
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.redirect("/burgers");
-  });
+});
 
-router.get("/burgers",  function(req, res){
-    burger.selectAll(function(data){
+router.get("/burgers", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
-            burger_data : data
+            burger_data: data
         };
         console.log(hbsObject);
         res.render("index.html", hbsObject)
+    })
+});
+
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        // Send back the ID of the new quote
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", function(req, res){
+    burger.update(req.params.id, function(result) {
+    
+        console.log(result);
+    
     })
 })
 
